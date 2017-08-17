1f6c54772531a42e47715b89bd923b31de6bc937
lora.strizhakova@gmail.com <lora.strizhakova@gmail.com>
2017-08-17 16:17:23 +0300

    tests were added

--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -5,11 +5,12 @@ const BasePage = require('./BasePage');
 class TaskPage extends BasePage {
 
     get inputField()            {return $('input.new-todo'); }
+    get inputEdit()             {return $('input.edit'); }
     get oneItem()               {return $('section.main li'); }
     get listItems()             {return browser.elements('section.main li '); }
     get markDone()              {return $('input[type="checkbox"]'); }
     get markUndone()            {return $('li.completed input[type="checkbox"]'); }
-    get itemCounter()           {return $('span.todo_count strong'); }
+    get itemCounter()           {return $('footer.footer strong'); }
     get allFilter()             {return $('a[href="#/]'); }
     get activeFilter()          {return $('a[href="#/active"]');}
     get completedFilter()       {return $('a[href="#/completed"]');}
--- test/specs/task.spec.js
+++ test/specs/task.spec.js
@@ -7,9 +7,59 @@ describe('Check process of adding new tasks and editing', function () {
     let pages = new PageManager();
 
     it('should add new task to list', function () {
-        pages.taskPage.open();
+        pages.taskPage.open(); //open page and add 1 task by clicking enter
+        pages.taskPage.inputField.waitForVisible();
+        pages.taskPage.inputField.setValue('Task 1').keys("Enter");
+        pages.taskPage.inputField.setValue('').keys("Enter");
+        pages.taskPage.itemCounter.waitForVisible();
+        let counter = pages.taskPage.itemCounter.getText();
+        expect(counter).to.equal('1');
+
+        //Mark task as done:
+        pages.taskPage.markDone.click();
+        counter = pages.taskPage.itemCounter.getText();
+        expect(counter).to.equal('0');
+
+        //Click on link "active" and check that no tasks in list.
+        pages.taskPage.activeFilter.click();
+        let taskVisiblity = pages.taskPage.oneItem.isVisible();
+        expect(taskVisiblity).to.equal(false);
+
+        //Click on link "completed" and chack 1 task in list:
+        pages.taskPage.completedFilter.click();
+        taskVisiblity = pages.taskPage.oneItem.isVisible();
+        expect(taskVisiblity).to.equal(true);
+
+        //Edit task by clicking enter:
+        pages.taskPage.oneItem.doubleClick();
+        pages.taskPage.inputEdit.setValue('Task 1 was changed').keys("Enter");
+        let taskText = pages.taskPage.oneItem.getText();
+        expect(taskText).to.equal('Task 1 was changed');
+
+
+        ////Edit task by clicking out of text field
+        pages.taskPage.oneItem.doubleClick();
+        pages.taskPage.inputEdit.setValue('Task 1 was changed twice');
         pages.taskPage.inputField.click();
-        // browser.setValue('header input', '111');
+        taskText = pages.taskPage.oneItem.getText();
+        expect(taskText).to.equal('Task 1 was changed twice');
+
+        //Check task as undone
+        pages.taskPage.markUndone.click();
+        taskVisiblity = pages.taskPage.oneItem.isVisible();
+        expect(taskVisiblity).to.equal(false);
+        counter = pages.taskPage.itemCounter.getText();
+        expect(counter).to.equal('1');
+
+        //move to link active? 1 task in list:
+        pages.taskPage.activeFilter.click();
+        taskVisiblity = pages.taskPage.oneItem.isVisible();
+        expect(taskVisiblity).to.equal(true);
+
+        //
+
 
     });
+
+
 });
